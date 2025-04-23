c2721e1b311ef6c7fd8e555aef379193b15066ca
softwareguru99 <ndanucynthia62@gmail.com>
2025-04-23 19:13:45 +0300

    fcontent update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c7d2c5ec5e42d7031392409d528e717205ef3856',
+        'reference' => 'ad3d120b13b9e3fd13da02116a8f664a7098f1af',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -421,7 +421,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c7d2c5ec5e42d7031392409d528e717205ef3856',
+            'reference' => 'ad3d120b13b9e3fd13da02116a8f664a7098f1af',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
